a9161e468eee9b2de7b103e33452245cfc2005ea
Yoni Slotwiner <yonislots@gmail.com>
2017-09-30 18:46:04 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,9 +2,9 @@ var playlist = new Object({artistName: "song title"});
 
 function updatePlaylist(playlist, artistName, songTitle){
   var playlist = {
-    Slowdive: 'Alison', 
-    My Bloody Valentine: 'Sometimes', 
-    Phil Ochs: "Here's to the State of Mississippi"
+    'Slowdive': 'Alison',
+    'My Bloody Valentine': 'Sometimes',
+    'Phil Ochs': "Here's to the State of Mississippi"
   };
   return playlist;
 };

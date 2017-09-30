4820f76ac3a5e9af8219d84fffaaf7e97e214749
Yoni Slotwiner <yonislots@gmail.com>
2017-09-30 19:10:26 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -6,6 +6,6 @@ function updatePlaylist(playlist, artistName, songTitle){
 };
 
 function removeFromPlaylist(playlist, artistName){
-  delete playlist[playlist.'artistName'];
+  delete playlist[playlist.artistName];
   return playlist;
 };
